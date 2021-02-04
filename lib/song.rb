f7fb60d5222de56ff4a9cd7b6deaca91fbf44192
Lexi Sandoval <lexi.sandoval9@gmail.com>
2021-02-04 18:55:10 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,8 +2,8 @@ require 'pry'
 
 class Song
 
-  extend Memorable
-  extend Findable
+  extend Memorable::ClassMethods
+  include Memorable::InstanceMethods  extend Findable
   include Paramable
 
   attr_accessor :name
